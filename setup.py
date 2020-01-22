e7d39ff48a6f09e0761f71e13d4a86914c27c35e
simecek <lamparna@gmail.com>
2020-01-22 21:48:18 +0100

    changes needed for Pypi

--- setup.py
+++ setup.py
@@ -19,11 +19,14 @@ class PyTest(TestCommand):
 
 setup(
     name='rbp',
-    version='0.1',
+    version='0.1.0',
     description='Python utils of RBP Bioinformatics.',
-    author='',
+    author='RBP Bioinformatics',
     author_email='lamparna@gmail.com',
-    url='',
+    license='MIT',
+    keywords=['bioinformatics', 'genome', 'dna'],
+    url='https://gitlab.com/RBP_Bioinformatics/rbp',
+    download_url='https://gitlab.com/RBP_Bioinformatics/rbp/-/archive/master/rbp-master.tar.gz',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     scripts=[],
@@ -44,4 +47,16 @@ setup(
     cmdclass={'test': PyTest},
     include_package_data=True,
     package_data={},
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
