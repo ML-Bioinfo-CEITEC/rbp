96dc8b0e6c2d353ea56a74e14d98a4926c6f4a0e
simecek <lamparna@gmail.com>
2020-02-18 23:46:12 +0100

    test_random_nucleotides fixed (seed set)

--- tests/random/test_random_sequence.py
+++ tests/random/test_random_sequence.py
@@ -2,6 +2,6 @@ from rbp.random.random_sequence import random_nucleotides
 
 
 def test_random_nucleotides():
-    expected_output = ["GGTACGGCGT", "GCTCCCGTGC"]
-    result = random_nucleotides(sample_size=2, seq_length=10)
+    expected_output = ['TTATCTGACT', 'AATTATGTAA']
+    result = random_nucleotides(sample_size=2, seq_length=10, seed=123)
     assert expected_output == result
